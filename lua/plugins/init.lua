b1bf1885ba7de3ed71d38c48a1a05fccf2de7f02
Juan Carlos <jcmrojas29@gmail.com>
2024-01-08 13:39:16 -0500

    add fugitive and extra plugins that requires only setup

---
+++ lua/plugins/init.lua
@@ -0,0 +1,30 @@
+return {
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup()
+        end
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end
+    },
+    {
+        'numToStr/Comment.nvim',
+        lazy = false,
+        config = function()
+            require("Comment").setup()
+        end
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require("lualine").setup()
+        end
+    }
+}
