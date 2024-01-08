cad4c5a7481d8a47a340cf53e2f9929148cd7986
Juan Carlos <jcmrojas29@gmail.com>
2024-01-08 12:05:00 -0500

    add harpoon and telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,26 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require('telescope.builtin')
+        local keymap = vim.keymap
+
+        require("telescope").setup {
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-u>"] = false
+                    },
+                },
+            }
+        }
+
+        keymap.set('n', '<leader>pf', builtin.find_files, {})
+        keymap.set('n', '<C-p>', builtin.git_files, {})
+        -- keymap.set('n', '<leader>ps', builtin.grep_string, {})
+        keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    end
+}
