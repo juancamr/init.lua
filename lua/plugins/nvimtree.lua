0fd687b0bb5f47efd289d40854f52d901065d1b8
Juan Carlos <jcmrojas29@gmail.com>
2024-01-09 12:50:21 -0500

    use nvimtree instead of netrw plugin

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		require("nvim-tree").setup({
+			view = {
+				width = 50,
+			},
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+		})
+	end,
+}
