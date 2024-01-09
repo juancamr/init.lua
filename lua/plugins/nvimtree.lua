b49afe2adf81884434389b97b91efbc98a2bdf24
Juan Carlos <jcmrojas29@gmail.com>
2024-01-09 13:00:16 -0500

    remap for nvim tree, <leader>pv

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -3,6 +3,9 @@ return {
 	config = function()
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
+
+		vim.keymap.set("n", "<leader>pv", "<cmd>NvimTreeToggle<CR>")
+
 		require("nvim-tree").setup({
 			view = {
 				width = 50,
