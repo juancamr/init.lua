7a9d954706477065a5565a1a507be538cac7f749
Juan Carlos <jcmrojas29@gmail.com>
2024-01-09 12:06:52 -0500

    add node_modules to ignore patterns in telescope configuration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,26 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require('telescope.builtin')
-        local keymap = vim.keymap
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		local keymap = vim.keymap
 
-        require("telescope").setup {
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-u>"] = false
-                    },
-                },
-            }
-        }
+		require("telescope").setup({
+			defaults = {
+				file_ignore_patterns = { "node_modules" },
+				mappings = {
+					i = {
+						["<C-u>"] = false,
+					},
+				},
+			},
+		})
 
-        keymap.set('n', '<leader>pf', builtin.find_files, {})
-        keymap.set('n', '<C-p>', builtin.git_files, {})
-        -- keymap.set('n', '<leader>ps', builtin.grep_string, {})
-        keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+		keymap.set("n", "<C-p>", builtin.find_files, {})
+		keymap.set("n", "<leader>ps", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		keymap.set("n", "<leader>vh", builtin.help_tags, {})
+	end,
 }
