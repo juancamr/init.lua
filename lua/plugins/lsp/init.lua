a32b451a70741471f43d838e8ae84c809153d633
Juan Carlos <jcmrojas29@gmail.com>
2024-01-10 20:47:36 -0500

    Add lsp feature (lsp signature)

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,18 +1,26 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("plugins.lsp.config.lspconfig")
-        end
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "L3MON4D3/LuaSnip"
-        },
-        config = function()
-            require("plugins.lsp.config.cmp")
-        end
-    }
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.lsp.config.lspconfig")
+		end,
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
+		},
+		config = function()
+			require("plugins.lsp.config.cmp")
+		end,
+	},
 }
