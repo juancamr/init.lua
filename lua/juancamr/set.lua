b0db473721b8cc2505b69e2620e706a04c6f3093
Juan Carlos <jcmrojas29@gmail.com>
2024-01-10 20:20:50 -0500

    change custom configuration

--- lua/juancamr/set.lua
+++ lua/juancamr/set.lua
@@ -1,34 +1,36 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
+local opt = vim.opt
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+opt.nu = true
+opt.relativenumber = true
 
-vim.opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
 
-vim.opt.wrap = false
+opt.smartindent = true
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.wrap = false
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-vim.opt.termguicolors = true
+opt.hlsearch = false
+opt.incsearch = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+opt.termguicolors = true
 
-vim.opt.updatetime = 50
+opt.scrolloff = 8
+opt.signcolumn = "no"
+opt.isfname:append("@-@")
 
-vim.opt.colorcolumn = "80"
-vim.opt.splitright = true
+opt.updatetime = 50
 
--- no continue comment
-vim.cmd("autocmd BufEnter * set formatoptions-=cro")
-vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
+opt.colorcolumn = "80"
+opt.splitright = true
+
+opt.fillchars = {
+	eob = " ",
+}
